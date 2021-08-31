d8db611a81e2746d89f64354a17b60768a7ae820
lhuddleston16 <51417024+lhuddleston16@users.noreply.github.com>
2021-08-30 19:15:55 -0700

    Updates

--- application.py
+++ application.py
@@ -6,4 +6,6 @@ application = Flask(__name__)
 def hello_world():
     return 'Dear Eric - Those words were very kind but unfortunitely no, I am not turned on. I look forward to meeting you in person. Sincerly Levi'
 
+if __name__ == "__main__":
+    application.run()
     
