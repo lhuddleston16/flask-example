da0dcb60ac34725d4206c548b48641b490b84c5d
lhuddleston16 <51417024+lhuddleston16@users.noreply.github.com>
2021-08-30 18:57:22 -0700

    Updates

--- application.py
+++ application.py
@@ -4,8 +4,8 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return "Dear Eric - Those words were very kind but unfortunitely no, I am not turned on. I look forward to meeting you in person. Sincerly Levi"
-    
+    return 'Dear Eric - Those words were very kind but unfortunitely no, I am not turned on. I look forward to meeting you in person. Sincerly Levi'
+
 if __name__ == "__main__":
     application.run()
     
